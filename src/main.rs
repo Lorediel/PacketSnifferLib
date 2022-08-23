6a4f9da994eb10c39e753bb71822ba244078b786
Alessandro Gelsi <s303525@studenti.polito.it>
2022-08-23 14:45:25 +0200

    working dns sniffer. Including info into report(id,opcode,responsecode, q.name, a.name)

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,9 @@ fn main() {
 
     let mut p = PacketCatcher::new();
     //Fare filtri manualmente perché pcap non funzionano
-    p.capture("\\Device\\NPF_{434FE10D-2348-48BF-9823-09CD95698329}", "rslts", 2, None);
-    //PacketCatcher::parse_network_adapter();
-    thread::sleep(Duration::from_secs(2));
+    //p.capture("\\Device\\NPF_{434FE10D-2348-48BF-9823-09CD95698329}", "rslts", 2, None);
+    p.capture("en0", "rslts", 2, None);    //PacketCatcher::parse_network_adapter();
+    thread::sleep(Duration::from_secs(5));
     p.switch(true);
     println!("qui");
     p.empty_report();
